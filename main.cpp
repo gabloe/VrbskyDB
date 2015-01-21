04f9ec61e3195a480780858c7cc0faf8ab44d8f2
paranoidgabe <gloewen@crimson.ua.edu>
2015-01-20 23:35:24 -0600

    added some comments

--- main.cpp
+++ main.cpp
@@ -5,13 +5,18 @@
 int
 main(int argc, char *argv[])
    {
-   if (argc < 2)
+   // Validate the number of arguments
+   if (argc != 2)
       {
       std::cerr << "usage: " << argv[0] << " <FILENAME>" << std::endl;
       exit(1);
       }
+
+   // Load the database
    DBase *db = new DBase(std::string(argv[1]));
    std::cout << "Loading Database: `" << argv[1] << "'" << std::endl; 
+
+   // Interactive queries
    std::string query;
    while (1)
       {
@@ -24,6 +29,8 @@ main(int argc, char *argv[])
       std::cout << "Query: " << query << std::endl;
       db->query(query);
       }
+
+   // Destroy all the stuff
    delete db;
    return 0;
    }
