cbc4b6f415a3677427a4c68c329327f4f617a76c
Jeffrey Robinson <deathly809@gmail.com>
2015-02-28 14:57:51 -0600

    MurmerHash

--- src/storage/LinearHash.cpp
+++ src/storage/LinearHash.cpp
@@ -682,6 +682,63 @@ void readFromFile( std::string filename , DataStructures::LinearHash<std::string
 	infile.close();
 }
 
+
+uint64_t MurmurHash64B ( const void * key, int len, unsigned int seed )
+{
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+
+	unsigned int h1 = seed ^ len;
+	unsigned int h2 = 0;
+
+	const unsigned int * data = (const unsigned int *)key;
+
+	while(len >= 8)
+	{
+		unsigned int k1 = *data++;
+		k1 *= m; k1 ^= k1 >> r; k1 *= m;
+		h1 *= m; h1 ^= k1;
+		len -= 4;
+
+		unsigned int k2 = *data++;
+		k2 *= m; k2 ^= k2 >> r; k2 *= m;
+		h2 *= m; h2 ^= k2;
+		len -= 4;
+	}
+
+	if(len >= 4)
+	{
+		unsigned int k1 = *data++;
+		k1 *= m; k1 ^= k1 >> r; k1 *= m;
+		h1 *= m; h1 ^= k1;
+		len -= 4;
+	}
+
+	switch(len)
+	{
+	case 3: h2 ^= ((unsigned char*)data)[2] << 16;
+	case 2: h2 ^= ((unsigned char*)data)[1] << 8;
+	case 1: h2 ^= ((unsigned char*)data)[0];
+			h2 *= m;
+	};
+
+	h1 ^= h2 >> 18; h1 *= m;
+	h2 ^= h1 >> 22; h2 *= m;
+	h1 ^= h2 >> 17; h1 *= m;
+	h2 ^= h1 >> 19; h2 *= m;
+
+	uint64_t h = h1;
+
+	h = (h << 32) | h2;
+
+	return h;
+} 
+
+
+size_t str_hash( std::string &str ) {
+	return MurmurHash64B( str.c_str() , str.size() , 123654789 );
+}
+
 int main(void) {
 	DataStructures::LinearHash<std::string> fromFile( 1024 , 16 );
 	DataStructures::LinearHash<std::string> myHash(1024, 16);
@@ -691,7 +748,7 @@ int main(void) {
 	std::string insert("ABCDEFGHI");
 	std::string fetch("ABCDEFGHI");
 	std::string file("ABCDEFGHI");
-	std::hash<std::string> str_hash;
+	//std::hash<std::string> str_hash;
 
 	clock_t start,end;
 	start = std::clock();
