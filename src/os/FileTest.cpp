583a181e64f25d47b5b802f5c12a3c6a6c6024c2
deathly809 <deathly809@gmail.com>
2015-04-02 19:17:52 -0500

    Fixed

---
+++ src/os/FileTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "FileSystem.h"
+#include "File.h"
+
+#include "FileWriter.h"
+
+int main( void ) {
+    const std::string FileName = "test.dat";
+    os::FileSystem fs( FileName );
+    os::File f = fs.open( "TEST" );
+    fs.shutdown();
+
+    return 0;
+}
