56ef30aa47540d0a70e4a75543e04ff6c19b302b
paranoidgabe <gloewen@crimson.ua.edu>
2015-04-18 23:19:03 -0500

    borked

---
+++ src/mmap_filesystem/ReadTest.cpp
@@ -0,0 +1,19 @@
+#include "Filesystem.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include "HashmapWriter.h"
+#include "HashmapReader.h"
+
+int main(void) {
+	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
+	for (int i=0; i<10; i++) {
+		std::string name(std::to_string(i));
+		File f = fs->open_file(name);
+		char *x = fs->read(&f);
+		std::string out(x, f.size);
+		std::cout << out << std::endl;
+		free(x);
+	}
+	fs->shutdown();
+}
---
+++ src/mmap_filesystem/WriteTest.cpp
@@ -0,0 +1,17 @@
+#include "Filesystem.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include "HashmapWriter.h"
+#include "HashmapReader.h"
+
+int main(void) {
+	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
+	std::string lorem("insert into test with {\"A\": 1};");
+	for (int i=0; i<100; i++) {
+		std::string name(std::to_string(i));
+		File f = fs->open_file(name);
+		fs->write(&f, lorem.c_str(), lorem.size());
+	}
+	fs->shutdown();
+}
