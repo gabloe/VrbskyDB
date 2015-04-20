a0dcd1ebd00ee89d11d6a1444562ffcaad19fdbd
deathly809 <deathly809@gmail.com>
2015-04-19 20:48:57 -0500

    idk

--- src/mmap_filesystem/WriteTest.cpp
+++ src/mmap_filesystem/WriteTest.cpp
@@ -5,6 +5,18 @@
 #include "HashmapWriter.h"
 #include "HashmapReader.h"
 
+void printFiles() {
+	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
+    auto files = fs->getFilenames();
+    for( auto n = files.begin() ; n != files.end() ; ++n ) {
+        std::cout << *n << std::endl;
+        File f = fs->open_file( *n );
+        std::cout << f.size << std::endl;
+    }
+	fs->shutdown();
+    delete fs;
+}
+
 void write( std::string prefix ) {
 	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
 	std::string lorem("{\"A\": 1};");
@@ -19,6 +31,8 @@ void write( std::string prefix ) {
 
 int main(void) {
     write( "herp" );
+    printFiles();
     write( "derp" );
+    printFiles();
     return 0;
 }
