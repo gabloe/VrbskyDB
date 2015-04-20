a58d66a0aac0b64485a726120a02fe1f809ba261
deathly809 <deathly809@gmail.com>
2015-04-20 18:27:04 -0500

    idk

--- src/mmap_filesystem/Filesystem.cpp
+++ src/mmap_filesystem/Filesystem.cpp
@@ -444,7 +444,7 @@ void Storage::Filesystem::writeMetadata() {
     uint64_t pos = 0;
     uint64_t files_size = 0;
 
-    printJunk();
+    //printJunk();
     
     uint64_t test = filesystem.numPages + metadata.firstFree + metadata.numFiles;
 
