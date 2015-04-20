2de363744ae359b1ae6b8ef309ba46b27bd5a286
Gabriel <gloewen@crimson.ua.edu>
2015-04-19 22:30:30 -0500

    Fixed filesystem reader

--- src/mmap_filesystem/FilesystemReader.cpp
+++ src/mmap_filesystem/FilesystemReader.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
-
-#define BLOCK_SIZE (3*sizeof(uint64_t)) + 64
-#define BLOCKS_PER_PAGE 128
+#include "Filesystem.h"
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -16,15 +14,17 @@ int main(int argc, char **argv) {
 	uint64_t bid = 0;
 	uint64_t usedSpace = 0;
 	uint64_t next = 0;
-	uint64_t numPages = 0;
-	uint64_t numFiles = 0;
-	uint64_t firstFree = 0;
+	uint64_t numPages;
+	uint64_t numFiles;
+	uint64_t firstFree;
+	char data[BLOCK_SIZE];
 	in.read(reinterpret_cast<char*>(&bid), sizeof(uint64_t));
 	in.read(reinterpret_cast<char*>(&usedSpace), sizeof(uint64_t));
 	in.read(reinterpret_cast<char*>(&next), sizeof(uint64_t));
-	in.read(reinterpret_cast<char*>(&numPages), sizeof(uint64_t));
-	in.read(reinterpret_cast<char*>(&numFiles), sizeof(uint64_t));
-	in.read(reinterpret_cast<char*>(&firstFree), sizeof(uint64_t));
+	in.read(data, BLOCK_SIZE);
+	memcpy(&numPages, data, sizeof(uint64_t));
+	memcpy(&numFiles, data + sizeof(uint64_t), sizeof(uint64_t));
+	memcpy(&firstFree, data + 2*sizeof(uint64_t), sizeof(uint64_t));
 	std::cout << "Metadata" << std::endl;
 	std::cout << "\tBlock ID: " << bid << std::endl;
 	std::cout << "\tUsed space: " << usedSpace << std::endl;
@@ -32,9 +32,18 @@ int main(int argc, char **argv) {
 	std::cout << "\t# pages: " << numPages << std::endl;
 	std::cout << "\t# files: " << numFiles << std::endl;
 	std::cout << "\tFirst free block: " << firstFree << std::endl;
+	std::cout << "\tData: " << data << std::endl;
 	for (uint64_t page = 0; page < numPages; ++page) {
 		for (uint64_t block=0; block < BLOCKS_PER_PAGE; ++block) {
-			
+			in.read(reinterpret_cast<char*>(&bid), sizeof(uint64_t));
+			in.read(reinterpret_cast<char*>(&usedSpace), sizeof(uint64_t));
+			in.read(reinterpret_cast<char*>(&next), sizeof(uint64_t));
+			in.read(data, BLOCK_SIZE);
+			std::cout << "Block:" << std::endl;
+			std::cout << "\tBlock ID: " << bid << std::endl;
+			std::cout << "\tUsed space: " << usedSpace << std::endl;
+			std::cout << "\tNext: " << next << std::endl;
+			std::cout << "\tData: " << data << std::endl;
 		}
 	}
 	in.close();
