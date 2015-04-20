ff3aa458e084bb05c78d6ae020a8e261d0239cfa
deathly809 <deathly809@gmail.com>
2015-04-19 21:49:49 -0500

    Updated tests

--- src/mmap_filesystem/ReadTest.cpp
+++ src/mmap_filesystem/ReadTest.cpp
@@ -6,22 +6,27 @@
 #include "HashmapWriter.h"
 #include "HashmapReader.h"
 
-int main(void) {
+int prefixRead(std::string prefix, int howMany) {
 	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
-    for (int i=0; i<20; i++) {
-        std::string name(std::to_string(i));
+    for (int i=0; i< howMany; i++) {
+        std::string name(prefix + std::to_string(i));
         File f = fs->open_file(name);
         if( f.size > 0 ) {
             std::cout << "Filesize: " << f.size << std::endl;
             char *x = fs->read(&f);
-            if  (x == NULL) {
-                continue;
+            if  (x != NULL) {
+                std::string out(x, f.size);
+                std::cout << out << std::endl;
+                free(x);
             }
-            std::string out(x, f.size);
-            std::cout << out << std::endl;
-            free(x);
         }
     }
     fs->shutdown();
     delete fs;
+}
+
+int main() {
+    prefixRead( "herp" , 10 );
+    prefixRead( "derp" , 10 );
+    return 0;
 }
