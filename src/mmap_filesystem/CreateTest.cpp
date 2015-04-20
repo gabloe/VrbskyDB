7806be0b68bc15f7b5d8270fd590e2c5fbbac710
deathly809 <deathly809@gmail.com>
2015-04-19 21:06:45 -0500

    lolwut

--- src/mmap_filesystem/CreateTest.cpp
+++ src/mmap_filesystem/CreateTest.cpp
@@ -2,6 +2,12 @@
 
 int main(void) {
 	Storage::Filesystem *fs = new Storage::Filesystem("data.db");
+    auto files = fs->getFilenames();
+    for( auto n = files.begin() ; n != files.end() ; ++n ) {
+        std::cout << *n << std::endl;
+        File f = fs->open_file( *n );
+        std::cout << f.size << std::endl;
+    }
 	fs->shutdown();
     delete fs;
     return 0;
