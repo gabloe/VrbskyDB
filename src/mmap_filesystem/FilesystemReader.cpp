a282e1598badce8d1028cafc704a9e78c5b4a41c
Gabriel <gloewen@crimson.ua.edu>
2015-04-19 21:37:19 -0500

    changed filesystem reader

--- src/mmap_filesystem/FilesystemReader.cpp
+++ src/mmap_filesystem/FilesystemReader.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "Filesystem.h"
-#include <string>
 #include <string.h>
-#include <vector>
+#include <fstream>
+
+#define BLOCK_SIZE (3*sizeof(uint64_t)) + 64
+#define BLOCKS_PER_PAGE 128
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -11,19 +12,31 @@ int main(int argc, char **argv) {
 	}
 
 	std::string fname(argv[1], strlen(argv[1]));
-	Storage::Filesystem *fs = new Storage::Filesystem(fname);
-	std::cout << "Files:" << std::endl;
-	std::vector<std::string> fnames = fs->getFilenames();
-	for (auto it = fnames.begin(); it != fnames.end(); ++it) {
-		std::cout << *it << ":" << std::endl;
-		File f = fs->open_file(*it);
-		std::cout << "\tStarts at block: " << f.block << std::endl;
-		std::cout << "\tFile size: " << f.size << std::endl;
-		char *data = fs->read(&f);
-		std::string out(data, f.size);
-		std::cout << "\tData: " << out << std::endl;
+	std::fstream in(fname, std::fstream::in | std::fstream::binary);
+	uint64_t bid = 0;
+	uint64_t usedSpace = 0;
+	uint64_t next = 0;
+	uint64_t numPages = 0;
+	uint64_t numFiles = 0;
+	uint64_t firstFree = 0;
+	in.read(reinterpret_cast<char*>(&bid), sizeof(uint64_t));
+	in.read(reinterpret_cast<char*>(&usedSpace), sizeof(uint64_t));
+	in.read(reinterpret_cast<char*>(&next), sizeof(uint64_t));
+	in.read(reinterpret_cast<char*>(&numPages), sizeof(uint64_t));
+	in.read(reinterpret_cast<char*>(&numFiles), sizeof(uint64_t));
+	in.read(reinterpret_cast<char*>(&firstFree), sizeof(uint64_t));
+	std::cout << "Metadata" << std::endl;
+	std::cout << "\tBlock ID: " << bid << std::endl;
+	std::cout << "\tUsed space: " << usedSpace << std::endl;
+	std::cout << "\tNext: " << next << std::endl;
+	std::cout << "\t# pages: " << numPages << std::endl;
+	std::cout << "\t# files: " << numFiles << std::endl;
+	std::cout << "\tFirst free block: " << firstFree << std::endl;
+	for (uint64_t page = 0; page < numPages; ++page) {
+		for (uint64_t block=0; block < BLOCKS_PER_PAGE; ++block) {
+			
+		}
 	}
-		
-	fs->shutdown();
-	delete fs;
+	in.close();
+	return 0;
 }
